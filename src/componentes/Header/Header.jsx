3c775a6fbd005cf24c722481401a87f4473c5c20
Samuel <samuelsantanadasilva8@gmail.com>
2023-07-05 19:12:41 -0300

    Atualizado

--- src/componentes/Header/Header.jsx
+++ src/componentes/Header/Header.jsx
@@ -5,12 +5,22 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
+//Imagens
+import Image from 'react-bootstrap/Image';
+
+
+
 function Header() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container fluid>
-        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+
+        <Navbar.Brand href="#">
+              <Image src="https://cdn-icons-png.flaticon.com/128/1829/1829892.png" alt='Logo' roundedCircle />
+        </Navbar.Brand>
+
         <Navbar.Toggle aria-controls="navbarScroll" />
+
         <Navbar.Collapse id="navbarScroll">
           <Nav
             className="me-auto my-2 my-lg-0"
@@ -18,21 +28,29 @@ function Header() {
             navbarScroll
           >
             <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Link</Nav.Link>
-            <NavDropdown title="Link" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+            <Nav.Link href="#action2">Increva-se</Nav.Link>
+
+            <NavDropdown title="Paginas" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">Cursos para desenvolvedores web e app</NavDropdown.Item>
               <NavDropdown.Item href="#action4">
-                Another action
+                    Cursos de Vendas e Persuasao
               </NavDropdown.Item>
-              <NavDropdown.Divider />
+
               <NavDropdown.Item href="#action5">
-                Something else here
+                    Cursos de Marketing Digital e Afiliados
+              </NavDropdown.Item>
+
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action">
+                    Cursos para Traders
               </NavDropdown.Item>
             </NavDropdown>
+
             <Nav.Link href="#" disabled>
-              Link
+                Contato
             </Nav.Link>
           </Nav>
+          
           <Form className="d-flex">
             <Form.Control
               type="search"
