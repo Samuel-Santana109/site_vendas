62ca6c386f8daf6f212b3010e283fe3e43021020
Samuel <samuelsantanadasilva8@gmail.com>
2023-08-12 13:27:09 -0300

    Subindo ultima atualizaçao

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
-import React from 'react';
+//import React from 'react';
 import './style/App.scss';
 
 import { Routeamento } from './Routeamento';
-import { BrowserRouter } from 'react-router-dom';
 
 //https://casapark.com.br/?gclid=Cj0KCQjw8e-gBhD0ARIsAJiDsaW5vEK36J8BHg2sX2DbJ4NtKx3HCL3fu6xGMY2RbTu0WWQJI0NRD74aAs4YEALw_wcB
 
 function App() { 
   return (
     <div className="App"> 
-        <BrowserRouter>
-          <Routeamento />
-        </BrowserRouter>  
-       
+          <Routeamento />       
     </div>
   )
 }
--- src/Routeamento.jsx
+++ src/Routeamento.jsx
@@ -1,26 +1,42 @@
-import {  Route, Routes } from "react-router-dom";
+//import React from 'react';
+import { BrowserRouter as Router,  Route, Routes,  } from 'react-router-dom';
 
-import Header from "./componentes/Header/Header";
+//import Header from "./componentes/Header/Header";
 import { Home } from './pages/Home';
 import { PaginaDevs } from './pages/desenvolvedores/PaginaDevs';
 import { Investimentos } from './pages/Investimentos/Investimentos';
-import Footer from './componentes/footer/Footer';
+//import Footer from './componentes/footer/Footer';
 
-
-export function Routeamento() { 
-    return (
-      <div > 
-      <Header />
-     
-          <Routes>
-            <Route index element={<Home />} />
-            <Route path="/paginaDevs" element={<PaginaDevs />} />
-            <Route path="/investimentos" element={<Investimentos />} />
-          </Routes>
-
-      <Footer/>
+// Define um componente chamado 'Routeamento'
+export function Routeamento() {
+  return (
+    // Um container div para envolver todo o conteúdo do componente
+    <div>
+      {/* Componente 'Router' que atua como o recipiente para as rotas */}
+      <Router>
+        {/* Componente 'Routes' para definir as rotas */}
+        <Routes>
+          {/* Define uma rota para o caminho raiz ('/') e renderiza o componente 'Home' */}
+          <Route exact path="/" element={Home} />
+          
+          {/* Define uma rota para o caminho '/PaginaDevs' e renderiza o componente 'PaginaDevs' */}
+          <Route path="/PaginaDevs" element={PaginaDevs} />
+          
+          {/* Define uma rota para o caminho '/contact' e renderiza o componente 'Investimentos' */}
+          <Route path="./pages/Investimentos/Investimentos" element={Investimentos} />
+        </Routes>
+      </Router>
     </div>
-    )
-  }
+  );
+}
+
+
+
+
+
+
+
+
+ 
 
 
