6db6a26d42f0f3cfeb95ba2cdb352b187562d7df
Samuel <samuelsantanadasilva8@gmail.com>
2023-11-14 13:34:37 -0300

    atualizando o pages

--- src/Routeamento.jsx
+++ src/Routeamento.jsx
@@ -1,36 +1,36 @@
 //import React from 'react';
-import { BrowserRouter as Router,  Route, Routes,  } from 'react-router-dom';
+// import { BrowserRouter as Router,  Route, Routes,  } from 'react-router-dom';
 
-import Header from "./componentes/Header/Header";
-import { Home } from './pages/Home/Home';
-import { PaginaDevs } from './pages/desenvolvedores/PaginaDevs';
-import { Investimentos } from './pages/Investimentos/Investimentos';
+// import Header from "./componentes/Header/Header";
+// import { Home } from './pages/Home/Home';
+// import { PaginaDevs } from './pages/desenvolvedores/PaginaDevs';
+// import { Investimentos } from './pages/Investimentos/Investimentos';
 
 
-// Define um componente chamado 'Routeamento'
- function Routeamento() {
-  return (
-    // Um container div para envolver todo o conteúdo do componente
-    <div>
-      {/* Componente 'Router' que atua como o recipiente para as rotas */}
-      <Router>
-        <Header />
-        {/* Componente 'Routes' para definir as rotas */}
-        <Routes>
-          {/* Define uma rota para o caminho raiz ('/') e renderiza o componente 'Home' */}
-          <Route path="/site_vendas/Home" element={<Home />} />
+// // Define um componente chamado 'Routeamento'
+//  function Routeamento() {
+//   return (
+//     // Um container div para envolver todo o conteúdo do componente
+//     <div>
+//       {/* Componente 'Router' que atua como o recipiente para as rotas */}
+//       <Router>
+//         <Header />
+//         {/* Componente 'Routes' para definir as rotas */}
+//         <Routes>
+//           {/* Define uma rota para o caminho raiz ('/') e renderiza o componente 'Home' */}
+//           <Route path="/site_vendas/Home" element={<Home />} />
           
-          {/* Define uma rota para o caminho '/PaginaDevs' e renderiza o componente 'PaginaDevs' */}
-          <Route path="/site_vendas/PaginaDevs" element={<PaginaDevs />} />
+//           {/* Define uma rota para o caminho '/PaginaDevs' e renderiza o componente 'PaginaDevs' */}
+//           <Route path="/site_vendas/PaginaDevs" element={<PaginaDevs />} />
           
-          {/* Define uma rota para o caminho '/contact' e renderiza o componente 'Investimentos' */}
-          <Route path="/site_vendas/Investimentos" element={<Investimentos />} />
-        </Routes>
-        <Footer />
-      </Router>
-    </div>
-  );
-}
+//           {/* Define uma rota para o caminho '/contact' e renderiza o componente 'Investimentos' */}
+//           <Route path="/site_vendas/Investimentos" element={<Investimentos />} />
+//         </Routes>
+//         <Footer />
+//       </Router>
+//     </div>
+//   );
+// }
 
 //Tem que pegar pelo meu caminho do package.json
 
